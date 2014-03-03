d4ccd8f05917579d8aa6e3ec0748057b15a57c74
geirhoe <geir.inge@gmail.com>
2014-03-03 23:17:50 +0100

    #5 La inn et hardcoded brett for hver vanskelighetsgrad

--- sudokufeud-core/src/main/java/no/lundesgaard/sudokufeud/engine/SimpleBoardGenerator.java
+++ sudokufeud-core/src/main/java/no/lundesgaard/sudokufeud/engine/SimpleBoardGenerator.java
@@ -6,19 +6,69 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SimpleBoardGenerator implements BoardGenerator {
+	public static final Integer N = null;
+
     @Override
     public Board generateBoard(Board.Difficulty difficulty) {
-        return new Board(difficulty,
-                null, null, 7, null, 6, 3, null, 9, 5,
-                null, null, 6, null, null, 9, null, 7, 2,
-                1, null, null, null, 5, 4, 3, 8, null,
+		switch (difficulty) {
+			case EASY:
+				return new Board(Board.Difficulty.EASY,
+						N,	N,	7,		N,	6,	3,		N,	9,	5,
+						N,	N,	6,		N,	N,	9,		N,	7,	2,
+						1,	N,	N, 		N,	5,	4,		3,	8,	N,
 
-                null, null, null, null, null, 2, null, null, null,
-                6, null, 2, null, 3, 8, 5, null, 9,
-                5, null, 4, 9, null, null, null, 3, 7,
+						N,	N,	N,		N,	N,	2,		N,	N,	N,
+						6,	N,	2,		N,	3,	8,		5,	N,	9,
+						5,	N,	4,		9,	N,	N,		N,	3,	7,
 
-                null, 9, 5, null, null, 1, null, 2, 8,
-                null, null, null, null, 9, null, null, null, null,
-                4, 3, null, null, 2, null, null, 5, 1);
+						N,	9,	5,		N,	N,	1,		N,	2,	8,
+						N,	N,	N,		N,	9,	N,		N,	N,	N,
+						4,	3,	N,		N,	2,	N,		N,	5,	1);
+
+			case MEDIUM:
+				return new Board(Board.Difficulty.MEDIUM,
+						9,	2,	N,		N,	5,	N,		7,	N,	N,
+						N,	N,	5,		N,	N,	2,		N,	N,	N,
+						N,	1,	6, 		N,	7,	N,		5,	3,	N,
+
+						N,	N,	N,		N,	N,	N,		2,	5,	N,
+						N,	4,	N,		5,	N,	3,		N,	9,	N,
+						N,	9,	2,		N,	N,	N,		N,	N,	N,
+
+						N,	7,	1,		N,	6,	N,		3,	4,	N,
+						N,	N,	N,		1,	N,	N,		8,	N,	N,
+						N,	N,	9,		N,	4,	N,		N,	2,	7);
+
+			case HARD:
+				return new Board(Board.Difficulty.HARD,
+						N,	6,	7,		1,	N,	N,		N,	N,	8,
+						9,	2,	N,		N,	N,	N,		N,	1,	4,
+						N,	N,	1, 		N,	N,	7,		N,	N,	N,
+
+						4,	N,	N,		N,	N,	N,		5,	6,	N,
+						N,	N,	N,		6,	N,	8,		N,	N,	N,
+						N,	3,	9,		N,	N,	N,		N,	N,	7,
+
+						N,	N,	N,		5,	N,	N,		3,	N,	N,
+						2,	9,	N,		N,	N,	N,		N,	7,	5,
+						5,	N,	N,		N,	N,	6,		9,	2,	N);
+
+			case EXPERT:
+				return new Board(Board.Difficulty.EXPERT,
+						N,	6,	N,		N,	5,	7,		N,	N,	2,
+						1,	N,	N,		9,	N,	N,		N,	N,	N,
+						9,	N,	N, 		N,	N,	N,		N,	4,	N,
+
+						7,	N,	2,		N,	4,	N,		N,	N,	N,
+						N,	N,	8,		N,	6,	N,		2,	N,	N,
+						N,	N,	N,		N,	7,	N,		9,	N,	8,
+
+						N,	1,	N,		N,	N,	N,		N,	N,	5,
+						N,	N,	N,		N,	N,	5,		N,	N,	1,
+						8,	N,	N,		6,	1,	N,		N,	2,	N);
+
+			default:
+				throw new RuntimeException("Unknown difficulty: " + difficulty);
+		}
     }
 }
